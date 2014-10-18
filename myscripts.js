397eb40c4527829137dacc65f8a911b84516bdf7
Christian Paul Dehli <cpdehli@gmail.com>
2014-10-18 13:39:15 -0400

    Adding javascript function to eventually hide images that aren't present.

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,9 @@
 function setup() {
 	var titleObject = document.getElementById("title");
 	titleObject.innerHTML = titleObject.innerHTML.toUpperCase ();
+	
+	var images = document.getElementsByTagName('img');
+	for (var i = 0; i < images.length; ++i) {
+		alert(images[i]);
+	}
 }
