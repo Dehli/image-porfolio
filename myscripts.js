fb957f586ab17ecbae0195c9f3e99b007cc02517
Christian Paul Dehli <cpdehli@gmail.com>
2014-10-23 20:09:17 -0400

    test.

--- myscripts.js
+++ myscripts.js
@@ -47,8 +47,8 @@ function setupHome() {
 		var newHeight = ratio * tallestHeightPixels;
 		var newWidth  = newHeight / heightOverWidth;
 		
-		sorted[i].parentNode.style.height = newHeight;
-		sorted[i].style.height = newHeight;
+		sorted[i].parentNode.height = newHeight;
+		//sorted[i].style.height = newHeight;
 		
 		//sorted[i].parentNode.style.width = newWidth;
 		//sorted[i].style.width = newWidth;
